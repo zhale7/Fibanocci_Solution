0a260e0c4dd4fd967f11e9e547d949dc14d75197
Jala <jala.f.hashimli@gmail.com>
2023-11-30 00:42:25 +0400

    Fibanocci Solution

---
+++ Fibanocci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibanocci_Solution
+{
+    public class Fibanocci
+    {
+        // constructor
+        public Fibanocci() { }
+
+        // Calculate Fibonici Series : 1,1,2,3,5,8,13...
+        public int Calculate(int Position)
+        {
+            if (Position <= 1) return Position;
+            else
+                return Calculate(Position - 1) + Calculate(Position - 2);
+        }
+    }
+}
